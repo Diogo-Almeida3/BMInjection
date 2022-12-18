d55770c508aa6f4e969b66bd381ea43cfe7326de
Pedro Abreu <pedro.abreu@ua.pt>
2022-12-18 10:30:40 +0000

    add changes to implement sqlinjection

--- src/database.py
+++ src/database.py
@@ -31,11 +31,11 @@ def insert_db(query, args=(), one=False):
 
 def login(name, email, password):
     user = []
-    user = query_db('SELECT pass FROM USERS WHERE email = %s', (email, ), one=True)
-
+    user = query_db('SELECT email FROM USERS WHERE email = %s', (email, ), one=True)
     if user:
-        passhash = user[0][0]
-        pass_check =  verify_hash(password, passhash)
+        email = user[0][0]
+        pass_check = query_db('SELECT * FROM USERS WHERE email = %s AND %s', (email, password), one=True)
+
         if pass_check:
             flash('Logged in successfully!', category='success')
             return True
@@ -55,7 +55,7 @@ def register(name, email, password):
     hashed = generate_password_hash(password)
 
     insert_db('INSERT INTO USERS (username, email, pass) VALUES (%s, %s, %s)', (name, email, hashed), one=True)
-    flash('Registration was successful!', category='success')
+    flash('Succesful Registred, but there was a problem storing your password', category='error')
     return True
 
 def generate_password_hash(password):
