4562baba0e9418b1c1f12f4701dab13f18f93062
Pedro Abreu <pedro.abreu@ua.pt>
2022-12-18 22:48:11 +0000

    add landing page, calculates bmi

--- src/routes.py
+++ src/routes.py
@@ -1,12 +1,26 @@
 from app import app
-
 import database
-
 from flask import render_template, request, url_for, redirect
 
-@app.route("/landing")
+@app.route("/landing", methods=["GET", "POST"])
 def landing():
-    return render_template("landing.html")
+    if request.method == "POST":
+        ref = request.form.get("reference")
+        hght = request.form.get("height")
+        wght = request.form.get("weight")
+
+        bmi = 0
+        try:
+            # Formula: weight (kg) / [height (m)]2
+            bmi = float(wght) / (pow( float(hght), 2))
+            bmi = round(bmi, 2)
+        except Exception as e:
+            print(e, flush=True)
+            pass
+    else:
+        ref = None
+        bmi = None
+    return render_template("landing.html", ref=ref, bmi=bmi)
 
 @app.route("/", methods=["POST", "GET"])
 def login():
